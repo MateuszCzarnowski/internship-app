fa8da17bbf87b1912c984c3b49e73b17e6d375bb
Mateusz Czarnowski <m.m.czarnowski@gmail.com>
2016-04-29 00:02:05 +0200

    Implemented pagination. Chart is now fully functional. Cosmetic changes.

--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,21 +1,33 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Investments</title>
+
+    <link href="<c:url value="/resources/css/bootstrap.min.css" />" rel="stylesheet">
     <link href="<c:url value="/resources/css/main.css" />" rel="stylesheet">
     <script src="<c:url value="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js" />"></script>
-    <script src="<c:url value="/resources/js/script.js" />"></script>
     <script src="<c:url value="/resources/js/jquery.canvasjs.min.js" />"></script>
+    <script src="<c:url value="http://paginationjs.com/dist/2.0.7/pagination.min.js" />"></script>
+    <script src="<c:url value="/resources/js/bootstrap.min.js" />"></script>
+    <script src="<c:url value="/resources/js/script.js" />"></script>
+
 </head>
 <body>
-<div id="chart">
-
+<div id="chart-container">
+    <div id="chart"></div>
+    <div id="date-range">
+        <input id="date1">
+        <input id="date2">
+        <button id="submit" class="button">Submit</button>
+    </div>
 </div>
-<div id="data">
-
+<div id="data-container">
+    <div id="data"></div>
+    <div id="pagination"></div>
 </div>
+
 </body>
 </html>
--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -1,42 +1,84 @@
 $(function () {
-    var data1 = "2015-07-01";
-    var data2 = "2016-01-01";
+    var date1 = $("#date1");
+    var date2 = $("#date2");
+
+    var chart = initializeChart();
+
+    date1.val("2015-01-01");
+    date2.val("2016-04-01");
+
+    sendRequest(chart, date1, date2);
+
+    $("#submit").click(function () {
+        sendRequest(chart, date1, date2);
+    });
+
+});
+
+
+function sendRequest(chart, date1, date2) {
     var resultList = [];
     $.ajax({
-        url: "/getData/" + data1 + "/" + data2,
+        url: "/getData/" + date1.val() + "/" + date2.val(),
         success: function (result) {
             var parsedResult = $.parseJSON(result);
             for (var obj in parsedResult) {
                 resultList.push({x: new Date(parsedResult[obj].date), y: parsedResult[obj].value});
             }
-            //console.log(resultList);
-            //$("#data").html(resultList);
-            console.log(resultList[0]);
-            var chart = new CanvasJS.Chart("chart",
-                {
 
-                    title: {
-                        text: "Investments rate over time"
-                    },
-                    axisY: {
-                        includeZero: false
-                    },
-                    data: [
-                        {
-                            type: "line",
-
-                            dataPoints: resultList
-                        }
-
-                    ]
-                });
-
+            chart.options.data[0].dataPoints = resultList;
             chart.render();
+
+            initializePagination(resultList);
+
         }
 
-
     });
+}
 
+function initializeChart() {
+    return new CanvasJS.Chart("chart",
+        {
 
-})
-;
+            title: {
+                text: "Investments rate over time"
+            },
+            axisY: {
+                includeZero: false
+            },
+            data: [
+                {
+                    type: "line",
+
+                    dataPoints: null
+                }
+
+            ]
+        }
+    );
+}
+
+function initializePagination(resultList) {
+    $('#pagination').pagination({
+        dataSource: resultList,
+        pageSize: 20,
+        formatResult: function (data) {
+            for (var i = 0, len = data.length; i < len; i++) {
+                data[i].x = data[i].x.toString().substring(4, 15);
+            }
+        },
+        callback: function (data, pagination) {
+            var html = simpleTemplating(data);
+            $("#data").html(html);
+        }
+    });
+}
+
+function simpleTemplating(data) {
+    var html = '<table class="table table-striped table-bordered"><thead><th>Date</th><th>Value</th></thead><tbody>';
+    $.each(data, function (index, item) {
+        html += '<tr><td>' + item.x + '</td><td> ' + item.y + '</td></tr>';
+    });
+    html += '</tbody></table>';
+    return html;
+}
