d861e103a37427c4d91eadf4ff115d18719f3085
Mateusz Czarnowski <m.m.czarnowski@gmail.com>
2016-04-28 18:02:25 +0200

    Added controller mapping for json response.

--- src/main/java/com/example/internship/controller/PageController.java
+++ src/main/java/com/example/internship/controller/PageController.java
@@ -2,9 +2,13 @@ package com.example.internship.controller;
 
 import com.example.internship.dao.InvestmentDAO;
 import com.example.internship.domain.Investment;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -27,6 +31,18 @@ public class PageController {
     @RequestMapping("/compare")
     public String compare() {
         return "compare";
+    }
+
+
+    @RequestMapping(value = "/getData/{date1}/{date2}", method = RequestMethod.GET)
+    @ResponseBody
+    public String sendData(@PathVariable String date1, @PathVariable String date2) {
+
+        List<Investment> investments = investmentDAO.getInvestmentsByDate(date1, date2);
+
+        String json = new Gson().toJson(investments);
+
+        return json;
     }
 
 }
